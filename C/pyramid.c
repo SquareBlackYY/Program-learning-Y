b520138cdc83c8987e419b4174651b992d47da9d
SquareBlackYY <1348076156zxy@gmail.com>
2023-02-08 13:49:34 +0800

    okok

--- C/pyramid.c
+++ C/pyramid.c
@@ -9,14 +9,10 @@ int main()
     for (cnt_2 = 0; cnt_2 < i; cnt_2++)
     {
         for (k = 0; k < cnt_3; k++)
-        {
             printf(" ");
-        }
         cnt_3 -= 1;
         for (cnt_0 = 0; cnt_0 < cnt_1; cnt_0++)
-        {
             printf("*");
-        }
         cnt_1 = cnt_4 * 2 - 1;
         printf("\n");
         cnt_4++;
