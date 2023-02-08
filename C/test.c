db261fc4448b2d3bcedbba64b87bbd0e129e93a3
SquareBlackYY <1348076156zxy@gmail.com>
2023-02-08 15:38:11 +0800

    okok

---
+++ C/test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("输入符号：");
+    scanf("%c",&ch);
+    //printf("%c",ch);
+
+    printf("   %c\n  %c%c%c\n %c%c%c%c%c\n%c%c%c%c%c%c%c",ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch,ch);
+    
+    
+}
