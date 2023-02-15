5cf77f2c598a981c11698bec3672c50b390a1913
SquareBlackYY <1348076156zxy@gmail.com>
2023-02-15 13:47:31 +0800

    ookok

---
+++ C/C语言程序设计-吕顺营/color.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main()
+{
+    printf("\033[31mThis is red.\n\033[0m");
+}
--- C/C语言程序设计-吕顺营/帅哥的.c
+++ C/C语言程序设计-吕顺营/帅哥的.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
+
 int main()
 {
     char ch;
     printf("输入一个符号以及层数：");
     int linenum,i,j,k;
     scanf("%c%d",&ch,&linenum);
-    //(1)行
     for (i=1;i<=linenum;i++)
     {
-        for(j=;;)
-
+        for(j=1;j<=linenum - i;j++)
+        printf(" ");
+        for(k = 1;k<=i * 2- 1;k++)
+        printf("%c", ch);
+    printf("\n");
     }
 }
