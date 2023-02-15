9d51b1e3cb17b6940d2ede5d12564432d395422c
SquareBlackYY <1348076156zxy@gmail.com>
2023-02-15 14:08:52 +0800

    okok

--- C/C语言程序设计-吕顺营/pyramid-str.c
+++ C/C语言程序设计-吕顺营/pyramid-str.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 int main()
 {
-    int i, k, cnt_0, cnt_1 = 1, cnt_2, cnt_3, cnt_4 = 2;
+    int i, k, cnt_0, cnt_1 = 1, cnt_2, cnt_3, cnt_4 = 2, firstline;
     char input;
     printf("输入金字塔符号：");
     scanf("%c", &input);
     printf("输入金字塔层数：");
     scanf("%d", &i);
+    printf("输入第一行个数：");
+    scanf("%d", &firstline);
     cnt_3 = i - 1;
     for (cnt_2 = 0; cnt_2 < i; cnt_2++)
     {
         for (k = 0; k < cnt_3; k++)
             printf(" ");
         cnt_3 -= 1;
-        for (cnt_0 = 0; cnt_0 < cnt_1; cnt_0++)
+        for (cnt_0 = 0; cnt_0 < cnt_1+firstline-1; cnt_0++)
             printf("%c", input);
         cnt_1 = cnt_4 * 2 - 1;
         printf("\n");
