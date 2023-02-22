e2278bade136d61dd0c052b96d5263f5939e4f77
SquareBlackYY <1348076156zxy@gmail.com>
2023-02-22 16:26:27 +0800

    okok

--- C/C语言程序设计-吕顺营/test.c
+++ C/C语言程序设计-吕顺营/test.c
@@ -3,62 +3,32 @@ int main ()
 {
 	char ch;
 	int i, j, k, h, lineNum, res;
-  
 	do
 	{	
 	    printf("请输入一个符号、首行符号个数和行数：");
-		
-        scanf("%c%d%d",&ch,&i,&lineNum);
-        //清空输入缓冲区
-        while(getchar()!= '\n');
-        //while(getchar() != EOF);
-        //fflush(stdin);        
-	}while(res<3);
-
-	/*
-	while(res<3)
-	{
-		printf("您的输入有误，请重新输入："); 
-		fflush(stdin); 
-		res=scanf("%c%d%d",&ch,&i,&lineNum);
-	}*/	    	
-	
+        res=scanf("%c%d%d",&ch,&i,&lineNum);
+        while(getchar() != '\n');
+	}while(res<3);	    	
 	if(lineNum==1)
 	{
 		if(i>80)
 		{
 			printf("超过了");
 			return 0;
-			
 		}
-		else  
-		if(i<=80)
-		{
+		else if(i<=80)
 			for(h=1;h<=i;h++)
-			{
-			 printf("%c",ch);
-			 
-			}
-			
-		}
-	
+				printf("%c",ch);
 	}
 	else if(lineNum!=1)
-    	{
+    {
 		for(h=1;h<=lineNum;h++)
 		{
-		for(j=0;j<=lineNum-h;j++)
-		    {
-			printf(" ");
-	    	}
-		for(k=1;k<=i+2*h-2;k++)
-		    {
-		     printf("%c",ch);
-	      	}
+			for(j=0;j<=lineNum-h;j++)
+				printf(" ");
+			for(k=1;k<=i+2*h-2;k++)
+				printf("%c",ch);
 		printf("\n");
-		
 		}
-    	}
-	 
-	
+    }
 }
