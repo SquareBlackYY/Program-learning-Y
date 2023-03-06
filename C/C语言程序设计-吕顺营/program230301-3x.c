8a3d76eae8a05455a32dbf5febecfa3b51798ef2
SquareBlackYY <1348076156zxy@gmail.com>
2023-03-06 17:56:01 +0800

    okok

--- C/C语言程序设计-吕顺营/program230301-3x.c
+++ C/C语言程序设计-吕顺营/program230301-3x.c
@@ -1,13 +1,82 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+//选择计算范围
 int main()
 {
+    int UpperLimit, a, b, i,OperatorNum, answer, CheckAnswer, Wrong, Remainder;
+    printf("请选择计算式子的范围(10,100,1000):");
+    scanf ("%d",&UpperLimit);
+    //生成随机算数式
     srand(time(NULL));
-    int a, b = 0;
-    for(;b<=10;b++)
+    for(;;)
     {
-    a = rand()%100;
-    printf("%d\n",a);
+        for(i = 1, Wrong = 0;i <= 10;i++)
+        {
+            OperatorNum = rand() % 4 + 1;
+            a = rand() % (UpperLimit + 1);
+            for(;;)
+            {
+            b = rand() % (UpperLimit + 1);
+            if(b==0 && OperatorNum == 4)
+                continue;
+            else
+                break;
+            }
+            printf("%d", a);
+            if (OperatorNum == 1)
+                printf("+");
+            else if (OperatorNum == 2)
+                printf("-");
+            else if (OperatorNum == 3)
+                printf("*");
+            else 
+                printf("/");
+            printf("%d = ", b);
+            scanf("%d",&answer);
+            if(OperatorNum == 4)
+                scanf("%d",&Remainder);
+            if( OperatorNum == 1 && answer == a + b);
+            else if( OperatorNum == 2 && answer == a - b);
+            else if( OperatorNum == 3 && answer == a * b);
+            else if (OperatorNum == 4 && answer == a / b && Remainder == a % b);
+            else
+            {
+                printf("做错了请再试一遍！\n");
+                printf("%d", a);
+                if (OperatorNum == 1)
+                    printf("+");
+                else if (OperatorNum == 2)
+                    printf("-");
+                else if (OperatorNum == 3)
+                    printf("*");
+                else 
+                    printf("/");
+                printf("%d = ", b);
+                scanf("%d",&answer);
+                if( OperatorNum == 1 && answer == a + b);
+                else if( OperatorNum == 2 && answer == a - b);
+                else if( OperatorNum == 3 && answer == a * b);
+                else if (OperatorNum == 4 && answer == a / b);
+                else
+                {
+                    Wrong++;
+                    printf("很遗憾，您两次都未回答正确！\n");
+                }
+            }
+        }
+    
+    printf("共10题，正确%d题，错误%d题。\n", 10 - Wrong, Wrong);
+    printf("是否要继续训练？（y/n）:");
+    char choose;
+    getchar();
+    scanf("%c",&choose);
+    if(choose=='y')
+        continue;
+    else
+    {
+        printf("拜拜，下次见哦！\n");
+        break;
+    }
     }
 }
