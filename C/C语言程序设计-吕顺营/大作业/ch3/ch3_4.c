985f6fade806fc4a9d763cdf5ee8fd0d4b4df1a9
SquareBlackYY <1348076156zxy@gmail.com>
2023-05-21 14:47:22 +0800

    okook

--- C/C语言程序设计-吕顺营/大作业/ch3/ch3_4.c
+++ C/C语言程序设计-吕顺营/大作业/ch3/ch3_4.c
@@ -6,9 +6,12 @@
 void ch3_4()
 {
     double a, b, c, x1, x2;
+
     printf("输入一元二次方程中的a,b,c：");
     scanf("%lf %lf %lf", &a, &b, &c);
+
     x1 = (-b - pow(b * b - 4 * a * c, 0.5)) / 2 * a;
     x2 = (-b + pow(b * b - 4 * a * c, 0.5)) / 2 * a;
+    
     printf("一元二次方程的根为：x₁=%.2f,x₂=%.2f\n", x1, x2);
 }
--- C/C语言程序设计-吕顺营/大作业/ch3/ch3_5.c
+++ C/C语言程序设计-吕顺营/大作业/ch3/ch3_5.c
@@ -7,9 +7,12 @@ const double pai = 3.14;
 void ch3_5()
 {
     double r, s, v;
+
     printf("输入球的半径r：");
     scanf("%lf", &r);
+
     s = 4 * pai * r * r;
     v = 4.0 / 3 * pai * r * r * r;
-    printf("球的表面积s=%.2f, 体积v=%.2f\n", s, v);
+    
+    printf("球的表面积S=%.2f, 体积V=%.2f\n", s, v);
 }
