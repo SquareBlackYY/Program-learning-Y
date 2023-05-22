46de79c7dffaf9f15b3202255982f203f41306a3
SquareBlackYY <1348076156zxy@gmail.com>
2023-05-22 21:30:42 +0800

    okok

--- C/C语言程序设计-吕顺营/大作业/ch9/ch9_6.c
+++ C/C语言程序设计-吕顺营/大作业/ch9/ch9_6.c
@@ -1,6 +1,31 @@
 // ch9_6.c
 
+#include <stdio.h>
+
+float f1(float);
+float f2(float);
+float Integral(float (*)(float), float, float);
+
 void ch9_6()
 {
-    
+    printf("y1 = %f\n", Integral(f1, 0.0, 1.0));
+    printf("y2 = %f\n", Integral(f2, 0.0, 3.0));
+}
+
+float f1(float x)
+{
+    return x * x + 1;
+}
+
+float f2(float x)
+{
+    return x / (x * x + 1);
+}
+
+float Integral(float (*f)(float), float a, float b)
+{
+    float x, sum = 0, n = 0.0001;
+    for(x = a;x < b;x += n)
+        sum += f(x) * n;
+    return sum;
 }
