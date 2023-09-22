e4e1abe554c2e5da2409ea04aba69e681e2636d5
SquareBlackYY <1348076156zxy@gmail.com>
2023-09-22 11:13:36 +0800

    ok

--- Python/潘/实验报告1/4-猜数字小游戏.py
+++ Python/潘/实验报告1/4-猜数字小游戏.py
@@ -2,18 +2,13 @@ import os
 import random
 
 while True:
-
     os.system("cls")
     print("猜数字游戏\n范围是1~20的整数")
     num = random.randint(1, 20)
-
     while True:
-
         input_num = input("猜猜这个数：")
-
-        if str.isdigit(input_num):
+        if str.isdigit(input_num) and isinstance(eval(input_num), int):
             input_num = int(input_num)
-
             if num < input_num:
                 print("大了")
             elif num > input_num:
