087b6cdf1992a707f10e7b0a1c2a892f81417751
SquareBlackYY <1348076156zxy@gmail.com>
2023-09-22 10:44:54 +0800

    ok

--- Python/潘俊杉/实验报告1/4-猜数字小游戏.py
+++ Python/潘俊杉/实验报告1/4-猜数字小游戏.py
@@ -1,19 +1,29 @@
+import os
 import random
 
-print("猜数字游戏\n范围是1~20的整数")
-
-num = random.randint(1, 20)
-
 while True:
-    input_num = input("猜猜这个数：")
-    if str.isdigit(input_num):
-        input_num = int(input_num)
-        if num < input_num:
-            print("大了")
-        elif num > input_num:
-            print("小了")
+
+    os.system("cls")
+    print("猜数字游戏\n范围是1~20的整数")
+    num = random.randint(1, 20)
+
+    while True:
+
+        input_num = input("猜猜这个数：")
+
+        if str.isdigit(input_num):
+            input_num = int(input_num)
+
+            if num < input_num:
+                print("大了")
+            elif num > input_num:
+                print("小了")
+            else:
+                print("恭喜你，猜对喽！")
+                break
         else:
-            print("恭喜你，猜对喽！")
-            break
-    else:
-        print("格式错误！")
+            print("格式错误！")
+
+    choice = input("你要继续玩吗?(y/n)")
+    if choice != "y":
+        break
