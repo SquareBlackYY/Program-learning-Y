e2e6c6a8f50cf1eaceeab1dffacd74b2ec51738f
SquareBlackYY <1348076156zxy@gmail.com>
2023-09-25 14:01:55 +0800

    okok

--- Python/对称密码-向泽军/实验报告2/Hill密码.py
+++ Python/对称密码-向泽军/实验报告2/Hill密码.py
@@ -1,66 +1,78 @@
-import numpy as np
+import numpy
 
-def encrypt_hill_cipher(plaintext, key):
-    # 将明文转换为大写，并去除空格和标点符号
-    plaintext = plaintext.upper().replace(" ", "").replace(".", "").replace(",", "")
+# 加密函数
+def encrypt_hill_cipher(text, key_matrix):
+    # 将明文转换为数字列表
+    plain_nums = [ord(c) - ord('A') for c in text]
+    # 计算需要添加的填充字符个数
+    padding = len(plain_nums) % key_matrix.shape[0]
+    if padding > 0:
+        plain_nums += [0] * (key_matrix.shape[0] - padding)
+    # 将数字列表转换为矩阵
+    plain_matrix = numpy.array(plain_nums).reshape(-1, key_matrix.shape[0]).T
+    # 使用密钥矩阵对明文矩阵进行加密
+    encrypted_matrix = numpy.dot(key_matrix, plain_matrix) % 26
+    # 将加密后的矩阵转换为数字列表
+    encrypted_nums = encrypted_matrix.T.flatten().astype(int).tolist()
+    # 将数字列表转换为密文
+    text = ''.join(chr(num + ord('A')) for num in encrypted_nums)
+    return text
 
-    # 补齐明文长度为3的倍数
-    while len(plaintext) % 3 != 0:
-        plaintext += "X"
-
-    # 将明文分组为3个字母一组
-    groups = [plaintext[i:i+3] for i in range(0, len(plaintext), 3)]
-
-    # 加密每个分组
-    ciphertext = ""
-    for group in groups:
-        # 将分组转换为向量
-        vector = np.array([ord(ch) - ord("A") for ch in group])
-
-        # 使用密钥矩阵进行加密
-        encrypted_vector = np.dot(key, vector) % 26
-
-        # 将加密后的向量转换为字母
-        encrypted_group = "".join([chr(val + ord("A")) for val in encrypted_vector])
-
-        ciphertext += encrypted_group
-
-    return ciphertext
-
-def decrypt_hill_cipher(ciphertext, key):
+# 解密函数
+def decrypt_hill_cipher(text, key_matrix):
+    # 将密文转换为数字列表
+    encrypted_nums = [ord(c) - ord('A') for c in text]
+    # 将数字列表转换为矩阵
+    encrypted_matrix = numpy.array(encrypted_nums).reshape(-1, key_matrix.shape[0]).T
     # 计算密钥矩阵的逆矩阵
-    inv_key = np.linalg.inv(key)
-    inv_key = np.round(inv_key * np.linalg.det(key)).astype(int) % 26
+    key_inv = numpy.linalg.inv(key_matrix)
+    # 使用逆矩阵对密文矩阵进行解密
+    decrypted_matrix = numpy.dot(key_inv, encrypted_matrix) % 26
+    # 将解密后的矩阵转换为数字列表
+    decrypted_nums = decrypted_matrix.T.flatten().astype(int).tolist()
+    # 去除填充字符
+    decrypted_nums = [num for num in decrypted_nums if num != 0]
+    # 将数字列表转换为明文
+    decrypted_text = ''.join(chr(num + ord('A')) for num in decrypted_nums)
+    return decrypted_text
 
-    # 将密文分组为3个字母一组
-    groups = [ciphertext[i:i+3] for i in range(0, len(ciphertext), 3)]
 
-    # 解密每个分组
-    plaintext = ""
-    for group in groups:
-        # 将分组转换为向量
-        vector = np.array([ord(ch) - ord("A") for ch in group])
+def is_int_number(list):
+    for i in list:
+        if i.isdigit() and isinstance(i, int):
+            return True
+    return False
 
-        # 使用逆矩阵进行解密
-        decrypted_vector = np.dot(inv_key, vector) % 26
+# 主程序
 
-        # 将解密后的向量转换为字母
-        decrypted_group = "".join([chr(val + ord("A")) for val in decrypted_vector])
+# 设置密钥矩阵
+key_matrix = numpy.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
 
-        plaintext += decrypted_group
+while True:
+    key = input("请输入3*3密钥矩阵(空格分隔):").split()
+    print(key[0])
+    print(is_int_number(key))
+    if not len(key) == 9 or not is_int_number(key):
+        print("格式错误！")
+    else:
+        # 将数字赋值给矩阵
+        for i in range(3):
+            for j in range(3):
+                key_matrix[i][j] = int(key[i*3 + j])
+        break
 
-    return plaintext
+for row in key_matrix:
+        print(row)
 
-# 定义密钥矩阵
-key = np.array([[6, 24, 1], [13, 16, 10], [20, 17, 15]])
 
-# 明文
-plaintext = "There will be people willing to help you ward off all the evil in the world."
+# 输入明文
+text = input("请输入明文: ").upper()
 
-# 加密
-ciphertext = encrypt_hill_cipher(plaintext, key)
-print("密文:", ciphertext)
+# 加密明文
+text = encrypt_hill_cipher(text, key_matrix)
+print("加密后的密文: ", text)
 
-# 解密
-decrypted_plaintext = decrypt_hill_cipher(ciphertext, key)
-print("解密后的明文:", decrypted_plaintext)
+# 解密密文
+decrypted_text = decrypt_hill_cipher(text, key_matrix)
+print("解密后的明文: ", decrypted_text)
+
