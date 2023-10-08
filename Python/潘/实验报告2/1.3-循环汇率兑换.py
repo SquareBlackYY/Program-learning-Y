929ccb4f87200e6b03b7f64adf1ad8a2b7c1e94b
SquareBlackYY <1348076156zxy@gmail.com>
2023-10-08 10:44:14 +0800

    okok

---
+++ Python/潘/实验报告2/1.3-循环汇率兑换.py
@@ -0,0 +1,10 @@
+while True:
+    before_exchange = input("输入兑换前数额(人民币末尾加y，美元末尾加d):")
+    if before_exchange[-1] == 'y':
+        after_exchange = eval(before_exchange[0:-1]) / 6
+        print("{} 元(CNY) = {:.2f} 美元(USD)".format(before_exchange[0:-1], after_exchange))
+    elif before_exchange[-1] == 'd':
+        after_exchange = eval(before_exchange[0:-1]) * 6
+        print("{} 美元(USD) = {:.2f} 元(CNY)".format(before_exchange[0:-1], after_exchange))
+    else:
+        print("格式错误，重新输入")
