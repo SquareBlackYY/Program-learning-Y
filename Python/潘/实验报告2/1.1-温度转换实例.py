74d9392f1a600a9590dda0458a6a42ecc5f6abb2
SquareBlackYY <1348076156zxy@gmail.com>
2023-10-08 11:09:15 +0800

    okok

--- Python/潘/实验报告2/1.1-温度转换实例.py
+++ Python/潘/实验报告2/1.1-温度转换实例.py
@@ -1,8 +1,8 @@
 before_trans = input("输入转换前温度(摄氏度末尾加c，华氏度末尾加f):")
-if before_trans[-1] == 'c':
+if before_trans[-1] == "c":
     after_trans = eval(before_trans[0:-1]) * 1.8 + 32
     print("{} 摄氏度(℃) = {:.2f} 华氏度(℉)".format(before_trans[0:-1], after_trans))
-elif before_trans[-1] == 'f':
+elif before_trans[-1] == "f":
     after_trans = (eval(before_trans[0:-1]) - 32) / 1.8
     print("{} 华氏度(℉) = {:.2f} 摄氏度(℃)".format(before_trans[0:-1], after_trans))
 else:
