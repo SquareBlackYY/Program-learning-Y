83123c05a85ce4b8f378c48898068ca979ce2468
SquareBlackYY <1348076156zxy@gmail.com>
2023-10-08 11:23:51 +0800

    okok

--- Python/潘/实验报告2/2.3-海贝色等边三角形.py
+++ Python/潘/实验报告2/2.3-海贝色等边三角形.py
@@ -1,7 +1,13 @@
 import turtle as t
 
-t.pencolor("seashell")
+t.pencolor("gray")
+t.pensize(80)
+for _ in range(3):
+    t.forward(100)
+    t.left(120)
 
+t.pencolor("seashell")
+t.pensize(1)
 for _ in range(3):
     t.forward(100)
     t.left(120)
