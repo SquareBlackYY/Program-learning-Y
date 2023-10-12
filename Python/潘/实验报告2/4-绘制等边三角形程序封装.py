23ef0a8d8884779bf6f4fa06e02ee431f0aefb3b
SquareBlackYY <1348076156zxy@gmail.com>
2023-10-12 21:36:55 +0800

    okok

--- Python/潘/实验报告2/4-绘制等边三角形程序封装.py
+++ Python/潘/实验报告2/4-绘制等边三角形程序封装.py
@@ -1,13 +1,24 @@
 from turtle import *
 
-def drawTr(color):
-    
-    pencolor(color)
+def drawTr(pen_color, pen_size = 1):
+    color(pen_color)
+    pensize(pen_size)
+    for i in range(3):
+        seth(i * 120)
+        fd(50)
 
-    for _ in range(3):
-        fd(100)
-        left(120)
+color("grey")
+pensize(80)
+for i in range(3):
+    seth(i * 120)
+    fd(100)
 
 drawTr("green")
+goto(50, 0)
 drawTr("gold")
-drawTr("#2E8B57")
+penup()
+goto(25, 25 * (3**0.5))
+pendown()
+drawTr("seashell")
+
+done()
