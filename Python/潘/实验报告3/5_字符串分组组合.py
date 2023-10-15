598695f4f99e0937534ffa3c8260b03a6ec22c07
SquareBlackYY <1348076156zxy@gmail.com>
2023-10-15 15:20:28 +0800

    okok

--- Python/潘/实验报告3/5_字符串分组组合.py
+++ Python/潘/实验报告3/5_字符串分组组合.py
@@ -1,5 +1,3 @@
-s = "Python is an excellent language"
-s = s.split()
-print(s)
-result = s[0] + "+" + s[-1]
+s = "Python is an excellent language".split()
+result = '+'.join([s[0], s[-1]])
 print(result)
