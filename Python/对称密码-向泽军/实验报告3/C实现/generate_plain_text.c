4770fdb331274a8859cdc3b53b5757d6ea5b3f9e
SquareBlackYY <1348076156zxy@gmail.com>
2023-10-16 11:01:01 +0800

    okok

--- Python/对称密码-向泽军/实验报告3/C实现/generate_plain_text.c
+++ Python/对称密码-向泽军/实验报告3/C实现/generate_plain_text.c
@@ -2,22 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+int main()
+{
+    clock_t start_time, end_time;
+    double execution_time;
     const long num_chars = 75 * 1024 * 1024;
 
     srand(time(NULL));
 
     FILE *file = fopen("input.txt", "w");
 
-    for (long i = 0; i < num_chars; i++) {
+    start_time = clock();
+
+    for (long i = 0; i < num_chars; i++)
+    {
         int random_num = rand() % 16;
         char hex_char = (random_num < 10) ? ('0' + random_num) : ('a' + random_num - 10);
         fprintf(file, "%c", hex_char);
     }
 
+    end_time = clock();
+
     fclose(file);
 
-    printf("已生成 %ld 个十六进制字符并写入 input.txt\n", num_chars);
+    execution_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+
+    printf("已生成 %ld 个十六进制字符并写入 input.txt, 用时: %.2f s\n", num_chars);
 
     return 0;
 }
