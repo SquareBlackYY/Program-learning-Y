51db8fadfe5925c8f1fea5dcbb4787baa7c1356e
SquareBlackYY <1348076156zxy@gmail.com>
2023-10-20 18:00:16 +0800

    okok

--- Python/潘/实验报告4/2_求和比较.py
+++ Python/潘/实验报告4/2_求和比较.py
@@ -1,14 +1,14 @@
 import time
 
 start_time = time.time()
-sum_1 = sum(range(1, 100000001))
+sum_1 = sum(range(1, 10001))
 end_time = time.time()
 time_1 = end_time - start_time
 print("方法一的累加结果为: {}".format(sum_1))
 print("方法一的运行时间为: {:.4f}秒".format(time_1))
 
 start_time = time.time()
-sum_2 = (1 + 100000000) * 100000000 // 2
+sum_2 = (1 + 10000) * 10000 // 2
 end_time = time.time()
 time_2 = end_time - start_time
 print("方法二的累加结果为: {}".format(sum_2))
