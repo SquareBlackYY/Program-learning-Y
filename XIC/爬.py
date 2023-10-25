1deaa156a6edf6a2a92201eb8b0caa7799fe95be
SquareBlackYY <1348076156zxy@gmail.com>
2023-10-25 20:34:32 +0800

    okok

--- XIC/爬.py
+++ XIC/爬.py
@@ -2,25 +2,12 @@ import os
 import requests
 
 def download_file(url, save_path):
-    response = requests.head(url)
-    total_size = int(response.headers.get('content-length', 0))
-    block_size = 1024  # 1 KB
-    progress_bar_width = 40
-
     response = requests.get(url, stream=True)
-    with open(save_path, 'wb') as file:
-        progress_bar = '[' + ' ' * progress_bar_width + ']'
-        print(f'Downloading {save_path}: {progress_bar}', end='\r')
+    block_size = 1024  # 1 KB
 
-        downloaded_size = 0
+    with open(save_path, 'wb') as file:
         for data in response.iter_content(block_size):
             file.write(data)
-            downloaded_size += len(data)
-            progress = int(progress_bar_width * downloaded_size / total_size)
-            progress_bar = '[' + '#' * progress + ' ' * (progress_bar_width - progress) + ']'
-            print(f'Downloading {save_path}: {progress_bar}', end='\r')
-
-    print(f'Downloading {save_path}: Completed')
 
 # 主文件的URL
 main_file_url = 'https://kit-pro.fontawesome.com/releases/v6.4.2/css/pro.min.css'
@@ -48,7 +35,14 @@ for line in lines:
         associated_files.append(url)
 
 # 下载关联文件到与源文件相同的文件夹中
+total_files = len(associated_files)
+completed_files = 0
+
 for file_url in associated_files:
     file_name = file_url.split('/')[-1]
     save_path = os.path.join(folder_name, file_name)
     download_file(file_url, save_path)
+    completed_files += 1
+    print(f'Downloaded file {completed_files}/{total_files}')
+
+print('Download completed')
