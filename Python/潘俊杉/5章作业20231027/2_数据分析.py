db2a4b13591dfe1f5fc96da165435c78e2603648
SuqareBlackYY <1348076156zxy@gmail.com>
2023-10-29 11:06:19 +0800

    okok

--- Python/潘俊杉/5章作业20231027/2_数据分析.py
+++ Python/潘俊杉/5章作业20231027/2_数据分析.py
@@ -1,11 +1,14 @@
+def max_score(score):
+    return max(score)
 
+def average_score(score):
+    return sum(score) / len(score)
 
+def median_score(score):
+    return score[len(score) // 2] if len(score) % 2 == 1 else (score[len(score) // 2] + score[len(score) // 2 - 1]) / 2
 
-score = input("请输入成绩，空格隔开：").split()
+score = sorted(list(map(float, input("请输入成绩，空格隔开：").split())))
 
-print(score)
-'''
-print("最高分：", max(scores))
-print("平均分：", get_average_score(scores))
-print("中位数：", get_median_score(scores))
-'''
+print("最高分：", max_score(score))
+print("平均分：", average_score(score))
+print("中位数：", median_score(score))
