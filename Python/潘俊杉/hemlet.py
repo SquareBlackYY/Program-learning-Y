a34420b45ebf3fa3456ed8daa151366536b40d1a
SquareBlackYY <1348076156zxy@gmail.com>
2023-11-07 20:14:19 +0800

    okok

--- Python/潘俊杉/hemlet.py
+++
@@ -1,9 +0,0 @@
-text = open("input.txt").read()
-text = text.upper()
-text.replace(t,' ')
-ls = text.split()
-lt = list(set(ls))
-d = {}
-for word_in_text in lt:
-    d[word_in_text] = text.count(word_in_text)
-print(d)
