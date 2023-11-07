107d2faf55e5f431a35ffd67126df5c91e4ba1da
SquareBlackYY <1348076156zxy@gmail.com>
2023-11-07 20:15:51 +0800

    okok

--- Python/潘俊杉/6章作业/哈姆雷特人物统计.py
+++ Python/潘俊杉/6章作业/哈姆雷特人物统计.py
@@ -1,36 +1,19 @@
+#哈姆雷特词频统计
 import re
-from collections import Counter
-
-def count_character_occurrences(filename):
-    character_names = {
-        'Hamlet': '哈姆莱特',
-        'Claudius': '克劳狄斯',
-        'Polonius': '波洛捏斯',
-        'Ofelia': '奥菲利娅',
-        'Laertes': '雷欧提斯',
-        'Horatio': '霍拉旭',
-        'Rosencrantz': '罗生克兰',
-        'Guildenstern': '盖登思邓',
-        'Marcellus': '马赛洛'
-    }
-
-    with open(filename, 'r', encoding='utf-8') as file:
-        text = file.read()
-
-    # 使用正则表达式匹配人物名字
-    pattern = r'\b(?:' + '|'.join(character_names.values()) + r')\b'
-    matches = re.findall(pattern, text)
-
-    # 统计人物名字出现的次数
-    character_counts = Counter(matches)
-
-    # 按姓名的字母顺序排序
-    sorted_counts = sorted(character_counts.items(), key=lambda x: x[0])
-
-    return sorted_counts
-
-# 使用示例
-filename = 'Hamlet.txt'
-character_occurrences = count_character_occurrences(filename)
-for character, count in character_occurrences:
-    print(f'{character}: {count}')
+characters = {
+    "hamlet",
+    "claudius",
+    "polonius",
+    "ofelia",
+    "laertes",
+    "horatio",
+    "rosencrantz",
+    "guildenstern",
+    "marcellus"
+}
+count = {}
+txt = open('哈姆雷特.txt','r').read().lower()
+txt = re.findall(r'[a-z]+',txt)
+for i in characters:
+    count[i] = txt.count(i)
+print(dict(sorted(count.items())))
