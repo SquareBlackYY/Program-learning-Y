4c93bcb74ede0a7c877eb75f5d85a15d926e281f
SquareBlackYY <1348076156zxy@gmail.com>
2023-11-10 19:37:35 +0800

    okok

--- Python/潘俊杉/7章作业/文本处理.py
+++ Python/潘俊杉/7章作业/文本处理.py
@@ -4,43 +4,36 @@ import csv
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
-with open("野猪公主.txt", "r", encoding="utf-8") as text:
-    words = jieba.lcut(re.sub(r"[^\u4e00-\u9fa5]", "", text.read()))
+with open("野猪公主.txt", "r", encoding="utf-8") as file:
+    words = jieba.lcut(re.sub(r"[^\u4e00-\u9fa5]", "", file.read()))
 
-with open("stop_words.txt", "r", encoding="utf-8") as text:
-    stop_words = text.read()
+with open("stop_words.txt", "r", encoding="utf-8") as file:
+    stop_words = file.read()
 
-item = list(set(words))
 word = {}
 for i in words:
-    if i not in stopwords:
+    if i not in stop_words:
         word[i] = word.get(i, 0) + 1
-result = sorted(word.items(), key=lambda x: x[1], reverse=True)
+word = dict(sorted(word.items(), key=lambda x: x[1], reverse=True))
 
-items = {}
-for word, count in result:
-    items[word] = count
-
-with open("results.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    for row in items.items():
+with open("results.csv", "w", encoding="utf-8", newline="") as file:
+    writer = csv.writer(file)
+    for row in word.items():
         writer.writerow(row)
 
-num = 0
-for word in items:
-    if items.get(word) >= 50:
-        num += 1
-    else:
-        continue
+count = 0
+for key, value in word.items():
+    if value > 50:
+        count += 1
 
 wordcloud = WordCloud(
     background_color="white",
     font_path="C:/Windows/Fonts/simhei.ttf",
-    max_words=num,
+    max_words=count,
     max_font_size=100,
     width=500,
     height=500,
-).generate_from_frequencies(items)
+).generate_from_frequencies(word)
 plt.figure(figsize=(8, 8))
 plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
