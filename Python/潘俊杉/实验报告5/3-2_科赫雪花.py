9e452fd8ce7af9746b650de709274f657ac70803
SquareBlackYY <1348076156zxy@gmail.com>
2023-11-10 17:49:41 +0800

    okok

--- Python/潘俊杉/实验报告5/3-2_科赫雪花.py
+++ Python/潘俊杉/实验报告5/3-2_科赫雪花.py
@@ -1,6 +1,5 @@
 import turtle
 
-
 def koch(size, n):
     if n == 0:
         turtle.fd(size)
@@ -13,14 +12,13 @@ def koch(size, n):
         turtle.left(60)
         koch(size / 3, n - 1)
 
-
 turtle.setup(800, 800)
 turtle.speed(20)
 turtle.penup()
 turtle.goto(-300, 200)
 turtle.pendown()
 turtle.pensize(2)
-n = eval(input("请输入要花的科赫雪花阶数："))
+n = eval(input("请输入科赫雪花阶数:"))
 for i in range(3):
     koch(500, n)
     turtle.right(120)
--- Python/潘俊杉/实验报告5/3-3_汉诺塔问题.py
+++ Python/潘俊杉/实验报告5/3-3_汉诺塔问题.py
@@ -6,6 +6,5 @@ def Hanoi(n, A, B, C):
         print("{} -> {}".format(A, C))
         Hanoi(n - 1, B, A, C)
 
-
 n = eval(input("请输入汉诺塔的层数："))
 Hanoi(n, "A", "B", "C")
