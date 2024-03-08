5a33e65808ed5002f35ff0c4c63f2be6d5ab0020
SquareBlackYY <1348076156zxy@gmail.com>
2024-03-08 16:40:32 +0800

    okok

---
+++ 24-1-数据结构-李胜华/线性表.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    const int MaxSize = 100;
+    typedef struct T1
+    {
+        int data[MaxSize];
+        int length;
+    } SqList;
+    return 0;
+}
