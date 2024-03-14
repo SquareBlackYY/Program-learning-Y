de4b385c59e8660495044d1dcbc6745e7392afc9
SquareBlackYY <1348076156zxy@gmail.com>
2024-03-14 11:28:51 +0800

    ok

--- 24-2-公钥密码-刘珍/实验一-RSA实现/3-中国剩余定理.cpp
+++ 24-2-公钥密码-刘珍/实验一-RSA实现/3-中国剩余定理.cpp
@@ -1,135 +1,104 @@
 #include <iostream>
-#include <gmp.h>
-using namespace std;
+#include <gmpxx.h>
 
 #define length 1
 
-void ExEuclid(mpz_t, mpz_t, mpz_t);
-void RSA_encrypt(mpz_t[], mpz_t[], mpz_t, mpz_t);
-void RSA_decrypt(mpz_t[], mpz_t, mpz_t, mpz_t, mpz_t);
+void ExEuclid(mpz_class&, mpz_class&, mpz_class&);
+void RSA_encrypt(mpz_class[], mpz_class[], mpz_class, mpz_class);
+void RSA_decrypt(mpz_class[], mpz_class, mpz_class, mpz_class, mpz_class);
 
 int main()
 {
-    mpz_t p, q, n, fn, e, d, p_1, q_1;
-    mpz_inits(p, q, n, fn, e, d, p_1, q_1, NULL);
+    mpz_class p(137);
+    mpz_class q(131);
+    mpz_class n = p * q;
 
-    mpz_set_ui(p, 137);
-    mpz_set_ui(q, 131);
+    mpz_class p_1 = p - 1;
+    mpz_class q_1 = q - 1;
+    mpz_class fn = p_1 * q_1;
 
-    mpz_mul(n, p, q);
+    mpz_class e(3);
+    mpz_class d;
 
-    mpz_sub_ui(p_1, p, 1);
-    mpz_sub_ui(q_1, q, 1);
-    mpz_mul(fn, p_1, q_1);
-
-    mpz_set_ui(e, 3);
-
-    // 辗转相除法求e的逆
+    // Extended Euclidean Algorithm to find the modular inverse of e
     ExEuclid(d, fn, e);
 
-    mpz_t m[length], c[length];
-    for (int i = 0; i < length; i++)
-    {
-        mpz_init(m[i]);
-        mpz_init(c[i]);
-    }
+    std::cout << d << std::endl;
 
-    mpz_set_ui(m[0], 513);
+    mpz_class m[length], c[length];
+    m[0] = 513;
 
-    // 加密
+    // Encryption
     RSA_encrypt(m, c, e, n);
 
-    // 输出加密结果
+    // Output encrypted result
     for (int i = 0; i < length; i++)
-        gmp_printf("%Zd ", c[i]);
-    cout << endl;
+        std::cout << c[i] << " ";
+    std::cout << std::endl;
 
-    // 解密
+    // Decryption
     RSA_decrypt(c, d, e, p, q);
-
-    for (int i = 0; i < length; i++)
-    {
-        mpz_clear(m[i]);
-        mpz_clear(c[i]);
-    }
-    mpz_clears(p, q, n, fn, e, d, NULL);
 
     return 0;
 }
 
-void ExEuclid(mpz_t result, mpz_t f, mpz_t d)
+void ExEuclid(mpz_class& result, mpz_class& f, mpz_class& d)
 {
-    mpz_t x, y, t, q;
-    mpz_inits(x, y, t, q, NULL);
-
-    mpz_set_ui(x, 1);
-    mpz_set_ui(y, 0);
-    mpz_set(t, f);
+    mpz_class x = 1, y = 0, t = f, q;
 
     while (1)
     {
-        mpz_fdiv_q(q, x, y);
-        mpz_submul(t, q, y);
-        mpz_set(x, y);
-        mpz_set(y, t);
+        q = x / y;
+        t -= q * y;
+        x = y;
+        y = t;
 
-        if (mpz_cmp_ui(y, 1) == 0)
+        if (y == 1)
         {
-            mpz_mod(result, y, f);
-            if (mpz_sgn(result) < 0)
-            {
-                mpz_add(result, result, f);
-            }
+            result = y % f;
+            if (result < 0)
+                result += f;
             break;
         }
-        else if (mpz_cmp_ui(y, 0) == 0)
+        else if (y == 0)
         {
-            mpz_set_ui(result, 0);
+            result = 0;
             break;
         }
     }
-
-    mpz_clears(x, y, t, q, NULL);
 }
 
-void RSA_encrypt(mpz_t m[], mpz_t c[], mpz_t e, mpz_t n)
+void RSA_encrypt(mpz_class m[], mpz_class c[], mpz_class e, mpz_class n)
 {
     for (int i = 0; i < length; i++)
-        mpz_powm(c[i], m[i], e, n);
+        c[i] = mpz_powm(m[i].get_mpz_t(), e.get_mpz_t(), n.get_mpz_t());
 }
 
-void RSA_decrypt(mpz_t c[], mpz_t d, mpz_t e, mpz_t p, mpz_t q)
+void RSA_decrypt(mpz_class c[], mpz_class d, mpz_class e, mpz_class p, mpz_class q)
 {
-    mpz_t m[length], q_inv, dp, dq, m1, m2, h;
-    mpz_inits(q_inv, dp, dq, m1, m2, h, NULL);
-
+    mpz_class q_inv, dp, dq, m1, m2, h;
     ExEuclid(q_inv, p, q);
 
-    mpz_sub_ui(dp, p, 1);
-    mpz_sub_ui(dq, q, 1);
+    dp = p - 1;
+    dq = q - 1;
 
     ExEuclid(dp, dp, e);
     ExEuclid(dq, dq, e);
 
     for (int i = 0; i < length; i++)
     {
-        mpz_init(m[i]);
+        mpz_class m1 = mpz_powm(c[i].get_mpz_t(), dp.get_mpz_t(), p.get_mpz_t());
+        mpz_class m2 = mpz_powm(c[i].get_mpz_t(), dq.get_mpz_t(), q.get_mpz_t());
 
-        mpz_powm(m1, c[i], dp, p);
-        mpz_powm(m2, c[i], dq, q);
+        h = m1 - m2;
+        h = abs(h);
+        h = h * q_inv % p;
 
-        mpz_sub(h, m1, m2);
-        mpz_abs(h, h);
-        mpz_mul(h, h, q_inv);
-        mpz_mod(h, h, p);
-
-        mpz_mul(h, h, q);
-        mpz_add(m[i], m2, h);
+        h = h * q;
+        m[i] = m2 + h;
     }
 
     for (int i = 0; i < length; i++)
-        gmp_printf("%Zd ", m[i]);
-    cout << endl;
-
-    mpz_clears(q_inv, dp, dq, m1, m2, h, NULL);
+        std::cout << m[i] << " ";
+    std::cout << std::endl;
 }
