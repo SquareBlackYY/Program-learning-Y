a53eba469212e71c47cfff64ea1ed89b11e45d1c
SquareBlackYY <1348076156zxy@gmail.com>
2024-03-20 00:12:48 +0800

    okok

--- 24-1-数据结构-李胜华/2-1-顺序表/SqList.cpp
+++ 24-1-数据结构-李胜华/2-1-顺序表/SqList.cpp
@@ -2,27 +2,32 @@
 #include <iostream>
 using namespace std;
 
+// 初始化线性表，构造一个空的线性表L。
 void InitList(SqList *&L)
 {
     L = (SqList *)malloc(sizeof(SqList));
     L->length = 0;
 }
 
+// 销毁线性表，释放为线性表L分配的内存空间。
 void DestroyList(SqList *&L)
 {
     delete L;
 }
 
+// 判断线性表是否为空表，若L为空表，则返回真，否则返回假。
 bool ListEmpty(SqList *L)
 {
     return (L->length == 0);
 }
 
+// 求线性表的长度，返回L中元素的个数。
 int ListLength(SqList *L)
 {
     return (L->length);
 }
 
+// 输出线性表，当线性表L不为空时顺序输出L中各元素值。
 void DispList(SqList *L)
 {
     for (int i = 0; i < L->length; i++)
@@ -30,6 +35,7 @@ void DispList(SqList *L)
     cout << endl;
 }
 
+// 按序号求线性表中元素，用e返回L中第i(1≤i≤n)个元素值。
 bool GetElem(SqList *L, int i, ElemType &e)
 {
     if (i < 1 || i > L->length)
@@ -38,6 +44,7 @@ bool GetElem(SqList *L, int i, ElemType &e)
     return true;
 }
 
+// 按元素值查找，返回L中第一个值为e相等的元素序号。
 int LocateElem(SqList *L, ElemType e)
 {
     int i = 0;
@@ -48,6 +55,7 @@ int LocateElem(SqList *L, ElemType e)
     return i + 1;
 }
 
+// 插入元素，在L的第i(1≤i≤n+1)个位置插入一个新元素e。
 bool ListInsert(SqList *&L, int i, ElemType e)
 {
     int j;
@@ -61,6 +69,7 @@ bool ListInsert(SqList *&L, int i, ElemType e)
     return true;
 }
 
+// 删除元素，删除L的第i(1≤i≤n)个元素，并用e返回该元素值。
 bool ListDelete(SqList *&L, int i, ElemType &e)
 {
     int j;
--- 24-1-数据结构-李胜华/2-1-顺序表/SqList.hpp
+++ 24-1-数据结构-李胜华/2-1-顺序表/SqList.hpp
@@ -10,14 +10,14 @@ typedef struct
     int length;
 } SqList;
 
-void InitList(SqList *&L);
-void DestroyList(SqList *&L);
-bool ListEmpty(SqList *L);
-int ListLength(SqList *L);
-void DispList(SqList *L);
-bool GetElem(SqList *L, int i, ElemType &e);
-int LocateElem(SqList *L, ElemType e);
-bool ListInsert(SqList *&L, int i, ElemType e);
-bool ListDelete(SqList *&L, int i, ElemType &e);
+void InitList(SqList *&L);                       // 初始化线性表，构造一个空的线性表L。
+void DestroyList(SqList *&L);                    // 销毁线性表，释放为线性表L分配的内存空间。
+bool ListEmpty(SqList *L);                       // 判断线性表是否为空表，若L为空表，则返回真，否则返回假。
+int ListLength(SqList *L);                       // 求线性表的长度，返回L中元素的个数。
+void DispList(SqList *L);                        // 输出线性表，当线性表L不为空时顺序输出L中各元素值。
+bool GetElem(SqList *L, int i, ElemType &e);     // 按序号求线性表中元素，用e返回L中第i(1≤i≤n)个元素值。
+int LocateElem(SqList *L, ElemType e);           // 按元素值查找，返回L中第一个值为e相等的元素序号。
+bool ListInsert(SqList *&L, int i, ElemType e);  // 插入元素，在L的第i(1≤i≤n+1)个位置插入一个新元素e。
+bool ListDelete(SqList *&L, int i, ElemType &e); // 删除元素，删除L的第i(1≤i≤n)个元素，并用e返回该元素值。
 
 #endif
