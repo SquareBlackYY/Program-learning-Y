8a783dc7bf1ff38dd4392bd75b5ff54a3918ac52
SquareBlackYY <1348076156zxy@gmail.com>
2024-03-26 21:39:23 +0800

    ok

--- 24-1-密码设计与分析-向泽军/Enocoro-128v2-最少活跃S盒/Enocoro.py
+++ 24-1-密码设计与分析-向泽军/Enocoro-128v2-最少活跃S盒/Enocoro.py
@@ -2,15 +2,14 @@
 
 from gurobipy import *
 
-ROUND = 4
+ROUND = 1
 LFSR_B = []
 OBJ = []
 ALL_B = []
 TMP = []
 ALL_TMP = []
 SBOX = [2, 7, 16, 29]
-# XOR[1] XOR[2]属于L函数部分
-XOR = [[31, 32, 0], [32, 2, 35], [33, 7, 36], [37, 16, 32], [38, 29, 33], [2, 6, 3], [7, 15, 16], [16, 28, 17]]
+XOR = [[31, 32, 0], [32, 2, 35], [33, 7, 36], [35, 36], [37, 16, 32], [38, 29, 33], [2, 6, 3], [7, 15, 16], [16, 28, 17]]
 CONSTRAINS = []
 
 # 变量名生成
@@ -48,22 +47,56 @@ with open("enocoro.lp", "w") as f:
     tmp_iter = get_tmp_generator()
 
     # 填充寄存器
-    for _ in range(39): # 临时修改为39，原值为34
+    for _ in range(35):
         LFSR_B.append(next(b_iter))
     get_obj()
 
     # 轮函数
-    for round in range(ROUND):
-        LFSR_B_next = [""] + LFSR_B[:31] + LFSR_B[32:] # 32:属于L函数部分
+    for _ in range(ROUND):
+        # 创建下一状态的寄存器，前31位右移，保留第32位及以后的元素
+        LFSR_B_next = [""] + LFSR_B[:31] + LFSR_B[32:]
+        # 计算差分传播
         for xor in XOR:
-            new_b = next(b_iter)
-            new_tmp = next(tmp_iter)
-            CONSTRAINS.append(LFSR_B[xor[0]] + " + " + LFSR_B[xor[1]] + " + " + new_b + " - 2 " + new_tmp + " >= 0")
-            CONSTRAINS.append(new_tmp + " - " + LFSR_B[xor[0]] + " >= 0")
-            CONSTRAINS.append(new_tmp + " - " + LFSR_B[xor[1]] + " >= 0")
-            CONSTRAINS.append(new_tmp + " - " + new_b + " >= 0")
-            LFSR_B_next[xor[2]] = new_b
-        LFSR_B = LFSR_B_next.copy()
+            # 三叉树
+            if len(xor) == 3:
+                if xor[0] >= 37:
+                    new_b = next(b_iter)
+                    new_tmp = next(tmp_iter)
+                    CONSTRAINS.append(LFSR_B_next[xor[0]] + " + " + LFSR_B[xor[1]] + " + " + new_b + " - 2 " + new_tmp + " >= 0")
+                    CONSTRAINS.append(new_tmp + " - " + LFSR_B_next[xor[0]] + " >= 0")
+                    CONSTRAINS.append(new_tmp + " - " + LFSR_B[xor[1]] + " >= 0")
+                    CONSTRAINS.append(new_tmp + " - " + new_b + " >= 0")
+                    LFSR_B_next[xor[2]] = new_b
+                elif xor[2] <= 33:
+                    new_b = next(b_iter)
+                    new_tmp = next(tmp_iter)
+                    CONSTRAINS.append(LFSR_B[xor[0]] + " + " + LFSR_B[xor[1]] + " + " + new_b + " - 2 " + new_tmp + " >= 0")
+                    CONSTRAINS.append(new_tmp + " - " + LFSR_B[xor[0]] + " >= 0")
+                    CONSTRAINS.append(new_tmp + " - " + LFSR_B[xor[1]] + " >= 0")
+                    CONSTRAINS.append(new_tmp + " - " + new_b + " >= 0")
+                    LFSR_B_next[xor[2]] = new_b
+                else:
+                    new_b = next(b_iter)
+                    new_tmp = next(tmp_iter)
+                    CONSTRAINS.append(LFSR_B[xor[0]] + " + " + LFSR_B[xor[1]] + " + " + new_b + " - 2 " + new_tmp + " >= 0")
+                    CONSTRAINS.append(new_tmp + " - " + LFSR_B[xor[0]] + " >= 0")
+                    CONSTRAINS.append(new_tmp + " - " + LFSR_B[xor[1]] + " >= 0")
+                    CONSTRAINS.append(new_tmp + " - " + new_b + " >= 0")
+                    LFSR_B_next.append(new_b)
+            # 线性变换
+            else:
+                new_b1 = next(b_iter)
+                new_b2 = next(b_iter)
+                new_tmp = next(tmp_iter)
+                CONSTRAINS.append(LFSR_B_next[xor[0]] + " + " + LFSR_B_next[xor[1]] + " + " + new_b1 + " + " + new_b2 + " - 3 " + new_tmp + " >= 0")
+                CONSTRAINS.append(new_tmp + " - " + LFSR_B_next[xor[0]] + " >= 0")
+                CONSTRAINS.append(new_tmp + " - " + LFSR_B_next[xor[1]] + " >= 0")
+                CONSTRAINS.append(new_tmp + " - " + new_b1 + " >= 0")
+                CONSTRAINS.append(new_tmp + " - " + new_b2 + " >= 0")
+                LFSR_B_next.append(new_b1)
+                LFSR_B_next.append(new_b2)
+        # 一轮结束后刷新寄存器状态，截取前34位元素
+        LFSR_B = LFSR_B_next.copy()[:35]
         get_obj()
 
     # 写入目标函数
