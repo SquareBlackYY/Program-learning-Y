6b414cbf68293a70b6fde8997156c8d434d61ab7
SquareBlackYY <1348076156zxy@gmail.com>
2024-03-28 20:34:38 +0800

    ok

--- 24-1-数据结构-李胜华/2-2-单链表/LinkList.cpp
+++ 24-1-数据结构-李胜华/2-2-单链表/LinkList.cpp
@@ -21,7 +21,7 @@ void DestroyList(LinkNode *&L)
 }
 
 bool ListEmpty(LinkNode *L)
-{    
+{
     return (L->next == NULL);
 }
 
@@ -29,7 +29,7 @@ int ListLength(LinkNode *L)
 {
     int n = 0;
     LinkNode *p = L;
-    while(p->next != NULL)
+    while (p->next != NULL)
     {
         n++;
         p = p->next;
@@ -40,5 +40,92 @@ int ListLength(LinkNode *L)
 void DispList(LinkNode *L)
 {
     LinkNode *p = L->next;
-    
+    while (p->next != NULL)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+bool GetElem(LinkNode *L, int i, ElemType &e)
+{
+    int j = 0;
+    LinkNode *p = L;
+    if (i <= 0)
+        return false;
+    while (j < i && p != NULL)
+    {
+        p = p->next;
+        j++;
+    }
+    if (p == NULL)
+        return false;
+    else
+    {
+        e = p->data;
+        return true;
+    }
+}
+
+int LocateElem(LinkNode *L, ElemType e)
+{
+    int i = 1;
+    LinkNode *p = L->next;
+    while (p != NULL && p->data != e)
+    {
+        p = p->next;
+        i++;
+    }
+    if (p == NULL)
+        return 0;
+    else
+        return i;
+}
+
+bool ListInsert(LinkNode *&L, int i, ElemType e)
+{
+    int j = 0;
+    LinkNode *p = L, *s;
+    if (i <= 0)
+        return false;
+    while (j < i - 1 && p != NULL)
+    {
+        j++;
+        p = p->next;
+    }
+    if (p == NULL)
+        return false;
+    else
+    {
+        s = (LinkNode *)malloc(sizeof(LinkNode));
+        s->data = e;
+        s->next = p->next;
+        p->next = s;
+        return true;
+    }
+    return false;
+}
+
+bool ListDelete(LinkNode *&L, int i, ElemType &e)
+{
+    int j = 0;
+    LinkNode *p = L, *q;
+    if (j < i - 1 && p != NULL)
+    {
+        j++;
+        p = p->next;
+    }
+    if (p == NULL)
+        return false;
+    else
+    {
+        q = p->next;
+        if (q == NULL)
+            return false;
+        e = q->data;
+        p->next = q->next;
+        free(q);
+        return true;
+    }
 }
--- 24-1-数据结构-李胜华/2-2-单链表/exp2-2.cpp
+++ 24-1-数据结构-李胜华/2-2-单链表/exp2-2.cpp
@@ -4,10 +4,54 @@ using namespace std;
 
 int main()
 {
-    LinkNode *L;
+    LinkNode *h;
     ElemType a[] = {'a', 'b', 'c', 'd', 'e'};
 
+    // (1) 初始化顺序表L
+    InitList(h);
 
+    // (2) 依次插入a、b、c、d、e元素
+    for (int i = 0; i < 5; i++)
+        ListInsert(h, i + 1, a[i]);
+
+    // (3) 输出顺序表L
+    cout << "顺序表L: ";
+    DispList(h);
+
+    // (4) 输出顺序表L的长度
+    cout << "顺序表L的长度: " << ListLength(h) << endl;
+
+    // (5) 判断顺序表L是否为空
+    if (ListEmpty(h))
+        cout << "顺序表L为空" << endl;
+    else
+        cout << "顺序表L不为空" << endl;
+
+    // (6) 输出顺序表L的第3个元素
+    ElemType e;
+    GetElem(h, 3, e);
+    cout << "顺序表L的第3个元素为: " << e << endl;
+
+    // (7) 输出元素a的位置
+    cout << "元素a的位置为: " << LocateElem(h, 'a') << endl;
+
+    // (8) 在第4个元素的位置上插入f元素
+    ListInsert(h, 4, 'f');
+
+    // (9) 输出顺序表L
+    cout << "插入元素后的顺序表L: ";
+    DispList(h);
+
+    // (10) 删除顺序表L的第3个元素
+    ElemType deletedElem;
+    ListDelete(h, 3, deletedElem);
+
+    // (11) 输出顺序表L
+    cout << "删除第3个元素后的顺序表L: ";
+    DispList(h);
+
+    // (12) 释放顺序表L
+    DestroyList(h);
 
     return 0;
 }
