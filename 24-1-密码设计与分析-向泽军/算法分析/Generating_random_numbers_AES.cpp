73cb73d6cc6e536ce7730ff744d98c85513a9f0e
SquareBlackYY <1348076156zxy@gmail.com>
2024-04-29 16:43:36 +0800

    ok

--- 24-1-密码设计与分析-向泽军/算法分析/Generating_random_numbers_AES.cpp
+++ 24-1-密码设计与分析-向泽军/算法分析/Generating_random_numbers_AES.cpp
@@ -11,35 +11,37 @@
 using namespace std;
 
 #define N23 8388608
-//2^(23)
+// 2^(23)
 #define N8 256
-#pragma intrinsic( __rdtsc )
+#pragma intrinsic(__rdtsc)
 
 unsigned short int plaintext[N23][4];
-
 
 __inline volatile unsigned long long read_tsc(void)
 {
 	return __rdtsc();
 }
 
-
-#define RAND(a,b) (((a = 36969 * (a & 65535) + (a >> 16)) << 16) + \
-	(b = 18000 * (b & 65535) + (b >> 16))  )
+#define RAND(a, b) (((a = 36969 * (a & 65535) + (a >> 16)) << 16) + \
+					(b = 18000 * (b & 65535) + (b >> 16)))
 
 void block_rndfill(unsigned char *buf, const int len)
 {
 	static unsigned long a[2], mt = 1, count = 4;
 	static unsigned char r[4];
-	int                  i;
+	int i;
 
-	if(mt) { mt = 0; *(unsigned long long*)a = read_tsc(); }
-
-	for(i = 0; i < len; ++i)
+	if (mt)
 	{
-		if(count == 4)
+		mt = 0;
+		*(unsigned long long *)a = read_tsc();
+	}
+
+	for (i = 0; i < len; ++i)
+	{
+		if (count == 4)
 		{
-			*(unsigned long*)r = RAND(a[0], a[1]);
+			*(unsigned long *)r = RAND(a[0], a[1]);
 			count = 0;
 		}
 
@@ -53,27 +55,26 @@ void main()
 	clock_t clock1 = clock();
 	__m128i AESkey[10];
 	__m128i rmm00;
-	block_rndfill((unsigned char*)(AESkey), sizeof(AESkey));
-	block_rndfill((unsigned char*)(&rmm00), sizeof(rmm00));
+	block_rndfill((unsigned char *)(AESkey), sizeof(AESkey));
+	block_rndfill((unsigned char *)(&rmm00), sizeof(rmm00));
 	ofstream file;
-	file.open("plaintext.bin",ios_base::out|ios_base::binary);
-	for(int j=0;j<N8;j++)
+	file.open("plaintext.bin", ios_base::out | ios_base::binary);
+	for (int j = 0; j < N8; j++)
 	{
-		for(int i=0;i<N23;i++)
+		for (int i = 0; i < N23; i++)
 		{
-			if ((i&0x1) == 0)
+			if ((i & 0x1) == 0)
 			{
-				rmm00 = _mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(rmm00,AESkey[0]),AESkey[1]), AESkey[2]),AESkey[3]),AESkey[4]),AESkey[5]), AESkey[6]),AESkey[7]), AESkey[8]),AESkey[9]);
+				rmm00 = _mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(_mm_aesenc_si128(rmm00, AESkey[0]), AESkey[1]), AESkey[2]), AESkey[3]), AESkey[4]), AESkey[5]), AESkey[6]), AESkey[7]), AESkey[8]), AESkey[9]);
 			}
-			unsigned long long* randnp = (unsigned long long*)(&rmm00);
-			*(unsigned long long *) plaintext[i] = *(randnp+(i&0x1));
+			unsigned long long *randnp = (unsigned long long *)(&rmm00);
+			*(unsigned long long *)plaintext[i] = *(randnp + (i & 0x1));
 		}
 		file.write((char *)plaintext, sizeof(plaintext));
 	}
 	file.close();
 	clock_t clock2 = clock();
-	cout<<"time used is "<<(double)(clock2 - clock1)/CLOCKS_PER_SEC<<endl;
-	cout<<"Done!"<<endl;
+	cout << "time used is " << (double)(clock2 - clock1) / CLOCKS_PER_SEC << endl;
+	cout << "Done!" << endl;
 	getchar();
-
 }
