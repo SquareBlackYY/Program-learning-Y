e304b6d004697db0f86229b970fb9b2d1516d686
SquareBlackYY <1348076156@qq.com>
2024-10-14 16:17:45 +0800

    ok

---
+++ 24-25-1-数字签名与认证-刘珍/PKC库测试/STS协议测试.cpp
@@ -0,0 +1,32 @@
+#include "pkc.h"
+
+#include <thread>
+using namespace std;
+
+int main() {
+    // 创建公共频道
+    PublicChannel channel;
+
+    // 创建 User 对象
+    User A("Alice", channel);
+    User B("Bob", channel);
+
+    // 启动线程，KDC 和 User 轮询消息
+    thread userAThread(&User::pollMessages, &A);
+    thread userBThread(&User::pollMessages, &B);
+
+    // 模拟发送消息
+    Message msg("admin", "Alice", "请求协商共享密钥", {(Payload){"协议信息", {{"协议", "STS协议"}, {"步骤", "0"}, {"目标", "Bob"}}}});
+    channel.sendMessage(msg);
+    this_thread::sleep_for(chrono::seconds(1));
+
+    // 打印状态
+    cout << A << endl;
+    cout << B << endl;
+
+    // 等待线程完成
+    userAThread.join();
+    userBThread.join();
+
+    return 0;
+}
